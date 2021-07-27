f8d713ae4fb55ab35205e3994fd7f3d0b99fcb9c
gcozzarellim <giancarlocozzarelli@gmail.com>
2021-07-27 17:39:26 +0200

    advanced-queries-Giancarlo

--- Solutions.sql
+++ Solutions.sql
@@ -1,3 +1,19 @@
 USE sakila;
 -- 1. List each pair of actors that have worked together.
+SELECT a.actor_id, b.actor_id
+FROM film_actor a, film_actor b
+WHERE a.film_id = b.film_id
+AND a.actor_id <> b.actor_id;
+
 -- 2. For each film, list actor that has acted in more films.
+
+WITH cte AS (
+	SELECT film_id,CONCAT(a1.first_name, ' ', a1.last_name) AS actor, COUNT(film_id) AS num_films
+	FROM actor a1
+    JOIN film_actor fa
+    ON a1.actor_id = fa.actor_id
+    GROUP BY film_id, actor)
+SELECT f.title AS film, c.actor, c.num_films 
+FROM cte c
+JOIN film f 
+ON f.film_id = c.film_id;
